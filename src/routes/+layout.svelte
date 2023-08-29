b6c90a7c226223d27e7604e84f1c859d05ad7632
jccaldw1 <jacobccaldwell@outlook.com>
2023-08-28 22:45:25 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    import Navbar from "./Navbar.svelte";
+
+</script>
+<div class="app">
+    <Navbar/>
+    <br/>
+    <main>
+        This is the main slot.
+        <slot/>
+        This is the end of the main slot.
+    </main>
+    <br/>
+    <footer>
+        This is the footer.
+    </footer>
+</div>
